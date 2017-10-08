bf7f1e133aee270482f0bf86f0e3d1406f3b66a0
Imobach Martin <imobach.martin@sap.com>
2017-10-08 15:49:33 +0200

    Substitute mocha with jasmine. Remove unused files. Deactivate some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'comma-dangle': 0
+    'comma-dangle': 0,
+    'no-multiple-empty-lines': 0,
+    'space-before-function-paren': 0
   }
 }
